a57987f7fd6d44a6ef217285d9f351865458e896
andrew <1062708758@qq.com>
2023-11-19 21:14:25 +0800

    feat: 接入 soku-games

--- src/utils/network-server-controller.ts
+++ src/utils/network-server-controller.ts
@@ -4,7 +4,14 @@ import { Socket } from 'socket.io';
 @GamePluginImpl('network-server-controller')
 export class NetworkServerController extends GamePlugin {
   bindGame(game: Game, extra?: { sockets: Socket[] }): void | Record<string, any> {
-    extra?.sockets.forEach(s => {
+    const isOk = extra?.sockets.map(() => false);
+    extra?.sockets.forEach((s, i) => {
+      s?.on('game-start', () => {
+        isOk[i] = true;
+        if (isOk.every(x => x)) {
+          game.start();
+        }
+      });
       s?.on('game-step', (stepStr: string) => {
         game.step(stepStr);
       });
