6dfed70fe693f5423ea2ba7840a6c61a951ca869
SokuRitszZ <1062708758@qq.com>
2024-05-05 11:43:12 +0800

    feat: should fail if leave the game

--- src/modules/socket/plugins/game-over-rate.ts
+++ src/modules/socket/plugins/game-over-rate.ts
@@ -1,4 +1,5 @@
 import { Game, GamePlugin, GamePluginImpl, LifeCycle } from '@soku-games/core';
+import { cloneDeep, omit, range } from 'lodash';
 import { RateService } from '@/modules/rate/service';
 import { Room } from '@/modules/socket/room';
 import { GameMode } from '@/modules/socket/types';
@@ -14,6 +15,7 @@ export { Extra as GameOverRateExtra };
 export class GameOverRatePlugin extends GamePlugin {
   bindGame(game: Game, extra: Extra): void | Record<string, any> {
     const { Room, RateService } = extra;
+    const copiedPlayers = cloneDeep(Room.Players.map(player => omit(player, ['Socket'])));
     if (Room.Mode !== GameMode.Match) return ;
     game.subscribe(LifeCycle.AFTER_END, (result: string) => {
       const results = result.split(';');
@@ -21,7 +23,7 @@ export class GameOverRatePlugin extends GamePlugin {
         const sign = str[0];
         const numStr = str.slice(1);
         const num = +numStr * (sign === '-' ? -1 : 1);
-        const player = Room.Players[index];
+        const player = copiedPlayers[index];
         const rate = await RateService.getOrCreateRate({
           GameId: Room.Game.Id,
           UserId: player.User.Id,
@@ -33,6 +35,19 @@ export class GameOverRatePlugin extends GamePlugin {
           Score: newScore,
         });
       });
+    });
+
+    Room.Players.forEach((player, index) => {
+      if (Room.Mode !== GameMode.Match) return;
+
+      const socket = player.Socket;
+      const listener = () => {
+        const results = range(Room.Game.PlayerCount).map(x => x === index ? '-5' : '+0');
+        game.end(results.join(';'));
+        socket.off('disconnect', listener);
+      };
+      socket.on('disconnect', listener);
+      game.subscribe(LifeCycle.AFTER_END, () => socket.off('disconnect', listener));
     });
   }
 }
